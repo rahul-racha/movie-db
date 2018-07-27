810b4e3a0d08f5c9c5d9e9bebbea600c10b7c314
rahul-racha <rahul_rachamalla@outlook.com>
2018-07-27 10:24:49 +0530

    Initial Commit

---
+++ homes-movie-db/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homes-movie-db
+//
+//  Created by Rahul Racha on 7/27/18.
+//  Copyright © 2018 Rahul Racha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ homes-movie-dbTests/homes_movie_dbTests.swift
@@ -0,0 +1,36 @@
+//
+//  homes_movie_dbTests.swift
+//  homes-movie-dbTests
+//
+//  Created by Rahul Racha on 7/27/18.
+//  Copyright © 2018 Rahul Racha. All rights reserved.
+//
+
+import XCTest
+@testable import homes_movie_db
+
+class homes_movie_dbTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
